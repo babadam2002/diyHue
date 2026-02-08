d3490f0a356ed4f18f905ecebada43ef6dde8360
babadam2002 <139929557+babadam2002@users.noreply.github.com>
2026-02-08 22:04:58 +0100

    Update Govee API base URL

--- BridgeEmulator/lights/protocols/govee.py
+++ BridgeEmulator/lights/protocols/govee.py
@@ -6,7 +6,7 @@ from typing import List, Dict, Any, Tuple
 
 logging = logManager.logger.get_logger(__name__)
 
-BASE_URL = "https://openapi.api.govee.com/router/api/v1"
+BASE_URL = "https://developer-api.govee.com"
 BASE_TYPE = "devices.capabilities."
 
 def get_headers() -> Dict[str, str]:
